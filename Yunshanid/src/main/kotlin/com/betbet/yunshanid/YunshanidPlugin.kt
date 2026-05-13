caf4fe0e70f174f51c5b959af6997d2ddbef00a4
sad25kag <sad25kag@gmail.com>
2026-05-13 17:51:48 +0700

    Update YunshanidPlugin.kt

--- Yunshanid/src/main/kotlin/com/betbet/yunshanid/YunshanidPlugin.kt
+++ Yunshanid/src/main/kotlin/com/betbet/yunshanid/YunshanidPlugin.kt
@@ -1,11 +1,14 @@
 package com.betbet.yunshanid
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.*
 
 @CloudstreamPlugin
-class YunshanidPlugin: Plugin() {
+class YunshanidPlugin : Plugin() {
+
     override fun load() {
-        registerMainAPI(YunshanidProvider())
+
+        registerMainAPI(
+            YunshanidProvider()
+        )
     }
 }
