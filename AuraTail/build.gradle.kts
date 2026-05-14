1413bd87012be6a67566c472fff17a0a479e2b34
sad25kag <sad25kag@gmail.com>
2026-05-14 16:19:27 +0700

    add

---
+++ AuraTail/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+cloudstream {
+    description = "AuraTail Donghua"
+    language = "id"
+    authors = listOf("BetbetMiro")
+
+    status = 1
+    tvTypes = listOf(
+        "AnimeMovie",
+        "OVA",
+        "Anime",
+    )
+
+    iconUrl =
+        "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://auratail.vip&size=%size%"
+}
