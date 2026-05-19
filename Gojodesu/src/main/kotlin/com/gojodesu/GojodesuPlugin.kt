0e70aea8d61214d86a1d0bc564dfbb7f49418704
sad25kag <sad25kag@gmail.com>
2026-05-19 22:39:58 +0700

    fix(gojodesu): remove unresolved Emturbovid reference

--- Gojodesu/src/main/kotlin/com/gojodesu/GojodesuPlugin.kt
+++ Gojodesu/src/main/kotlin/com/gojodesu/GojodesuPlugin.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
-
 @CloudstreamPlugin
 class GojodesuPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Gojodesu())
+        // Kita hanya register yang ada kodenya saja
         registerExtractorAPI(Kotakajaib())
-        registerExtractorAPI(Emturbovid())
     }
 }
