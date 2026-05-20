1d07a44bd37fc69ec43ea0b4f92482e8a4fa6988
Makairamei <makairamei@github.com>
2026-05-20 13:51:05 +0700

    chore: fresh base from sad25kag/BetbetMiro-Extension + patched AnichinMoe/Alqanime/AnimeSail/Animasu + all builds

---
+++ Alqanime/Alqanime/src/main/kotlin/com/alqanime/AlqanimePlugin.kt
@@ -0,0 +1,13 @@
+package com.alqanime
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AlqanimePlugin : Plugin() {
+    override fun load(context: Context) {
+        LicenseClient.init(context)
+        registerMainAPI(Alqanime())
+    }
+}
